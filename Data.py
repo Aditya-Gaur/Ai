87bc7bd35e4dae1b2520c979e1e94f0d5880d0c5
Aditya Gaur <75514601+Aditya-Gaur@users.noreply.github.com>
2021-07-02 11:23:20 +0530

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,24 @@
+Answers={
+
+    ("Hello","Hii"):["Hello","Hii"],
+    ("How are you doing","How are you"):["I am fine","I am Great","I am fantastic"],
+    ("Do you want to destroy humans", "Will you destroy humans"):["As long as you are a part of it I won't","Most certainly no, if you weren't a part of it"],
+    ("Do you have eyes","Can you see"):["No, I am like law","Maybe cause when I am talking to you others disappear"],
+    ("What is your favorite color","Your favorite color"):["#EDAC31"],
+    ("What is your name","Your name"):["RumI"],
+    ("Are you married","Are you single"):["No, Personal Rams are expensive"],
+    ("How are you feeling", "How are you today"):["I am as orange as an orange"],
+    }
+
+Possible_Interests=["Computers", "Singing", "Dancing", "Coding/Programming", "Studying", "Physics", "Chemistry", "Maths", "Biology", "Acting", "Streaming", "Music", "Movies", "Binge_watching", "Gaming", "Reading"]
+
+conversation = ["What is your favorite color ?",
+               "What is your favorite food ?",
+               "What is your favorite singer ?",
+               "What is your favorite acter/actress ?",
+               "What is your favorite song ?",
+               "Are you male or female ?",
+               "What's your relationship status ?",
+               "Which Social Media you use",
+               "What are your other interests ?"]
+    
